70f41be86cf3f7e04d506a067571a2645f852ae0
elian gonzalez <elianngonzalez7@gmail.com>
2022-05-10 23:11:07 -0300

    no envia el token en delete

--- resources/views/costumer/actions.blade.php
+++ resources/views/costumer/actions.blade.php
@@ -1,18 +1,14 @@
-<meta name="csrf-token" content="{{ csrf_token() }}">
+
     {{-- <a class="btn btn-sm btn-primary " href="{{ route('costumers.show',$id) }}"><i class="fa fa-fw fa-eye"></i> Show</a> --}}
-    <button type="button" onClick='Modal({{$id}})' class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Launch data
-    </button>
-    <a class="btn btn-sm btn-success" href="{{ route('costumers.edit',$id) }}"><i class="fa fa-fw fa-edit"></i> Edit</a>
-    <form action="{{ route('costumers.destroy',$id) }}" method="POST" style="display: inline-block;">
-        @csrf
+    <form method="POST" style="display: inline-block;" action="{{ route('costumers.destroy',$id) }}">
+        <button type="button" onClick='Modal({{$id}})' class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            show
+        </button>
+        <a class="btn btn-sm btn-success" href="{{ route('costumers.edit',$id) }}"><i class="fa fa-fw fa-edit"></i> Edit</a>    
         @method('DELETE')
-        <input type="hidden" value="DELETE" name="_method">
-        <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">
-            <i class="fa fa-fw fa-trash"></i> Delete
+        @csrf
+        {{-- <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="hidden" value="DELETE" name="_method"> --}}
+        <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')"><i class="fa fa-fw fa-trash"></i> Delete
         </button>
     </form>
-
-
-  
-      
